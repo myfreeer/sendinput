d5ece710dae58096c39bb04b966cf973d45c5607
myfreeer <myfreeer@users.noreply.github.com>
2018-12-22 20:29:26 +0800

    appveyor: update and deploy on tags

--- sendinput.c
+++ sendinput.c
@@ -9,17 +9,17 @@
 #endif // __GNUC__
 
 void EmitError(const unsigned short errorCode) {
-    MessageBox(NULL, ErrorInfo[errorCode], NULL,
-               MB_OK | MB_ICONEXCLAMATION | MB_SETFOREGROUND);
+  MessageBox(NULL, ErrorInfo[errorCode], NULL,
+             MB_OK | MB_ICONEXCLAMATION | MB_SETFOREGROUND);
 }
 
 // main entry
 int WINAPI WinMain(HINSTANCE UNUSED hInstance, HINSTANCE UNUSED hPrevInstance,
                    LPSTR lpCmdLine, int UNUSED nShowCmd) {
-    unsigned int cmdLineLength = (unsigned int) lstrlen(lpCmdLine);
-    if (cmdLineLength == 0) {
-        return 1;
-    }
-    ParseSendKeys(lpCmdLine, cmdLineLength);
-    return 0;
+  unsigned int cmdLineLength = (unsigned int) lstrlen(lpCmdLine);
+  if (cmdLineLength == 0) {
+    return 1;
+  }
+  ParseSendKeys(lpCmdLine, cmdLineLength);
+  return 0;
 }
--- sendinput_io.c
+++ sendinput_io.c
@@ -5,77 +5,77 @@
 #define BLOCK_SIZE 4096
 
 char *readStdIn(size_t *size) {
-    int c = 0;
-    char *buf = malloc(sizeof(char) * BLOCK_SIZE);
-    size_t bufSize = BLOCK_SIZE, currSize = 0;
-    while ((c = getchar()) != EOF) {
-        if (currSize + 1 > bufSize) {
-            if (!(buf = realloc(buf, bufSize + BLOCK_SIZE))) {
-                return NULL;
-            }
-            bufSize += BLOCK_SIZE;
-        }
-        buf[currSize++] = (char) c;
+  int c = 0;
+  char *buf = malloc(sizeof(char) * BLOCK_SIZE);
+  size_t bufSize = BLOCK_SIZE, currSize = 0;
+  while ((c = getchar()) != EOF) {
+    if (currSize + 1 > bufSize) {
+      if (!(buf = realloc(buf, bufSize + BLOCK_SIZE))) {
+        return NULL;
+      }
+      bufSize += BLOCK_SIZE;
     }
-    buf[currSize] = '\0';
-    *size = currSize;
-    return buf;
+    buf[currSize++] = (char) c;
+  }
+  buf[currSize] = '\0';
+  *size = currSize;
+  return buf;
 }
 
 char *readFile(const char *fileName, size_t *size) {
-    /* declare a file pointer */
-    FILE *filePtr = fopen(fileName, "rb");
-    /* quit if the file does not exist */
-    if (filePtr == NULL)
-        return NULL;
+  /* declare a file pointer */
+  FILE *filePtr = fopen(fileName, "rb");
+  /* quit if the file does not exist */
+  if (filePtr == NULL)
+    return NULL;
 
-    /* Get the number of bytes */
-    fseek(filePtr, 0, SEEK_END);
-    size_t fileSize = (size_t) ftell(filePtr);
+  /* Get the number of bytes */
+  fseek(filePtr, 0, SEEK_END);
+  size_t fileSize = (size_t) ftell(filePtr);
 
-    /* reset the file position indicator to
-    the beginning of the file */
-    fseek(filePtr, 0, SEEK_SET);
+  /* reset the file position indicator to
+  the beginning of the file */
+  fseek(filePtr, 0, SEEK_SET);
 
-    /* grab sufficient memory for the
-    buffer to hold the text */
-    char *buffer = calloc(fileSize + 1, sizeof(char));
+  /* grab sufficient memory for the
+  buffer to hold the text */
+  char *buffer = calloc(fileSize + 1, sizeof(char));
 
-    /* memory error */
-    if (buffer == NULL)
-        return NULL;
+  /* memory error */
+  if (buffer == NULL)
+    return NULL;
 
-    /* copy all the text into the buffer */
-    *size = fread(buffer, sizeof(char), fileSize, filePtr);
-    fclose(filePtr);
-    buffer[fileSize + 1] = '\0';
-    return buffer;
+  /* copy all the text into the buffer */
+  *size = fread(buffer, sizeof(char), fileSize, filePtr);
+  fclose(filePtr);
+  buffer[fileSize + 1] = '\0';
+  return buffer;
 }
 
 void EmitError(const unsigned short errorCode) {
-    fputs(ErrorInfo[errorCode], stderr);
+  fputs(ErrorInfo[errorCode], stderr);
 }
 
 #define HELP_TEXT "Sendinput utility\n" \
-"Usage: sendinput [-|<path to file>]\n" \
+"Usage: sendinput_io [-|<path to file>]\n" \
 "Parses sendinput command from stdin or file and exec it."
 
 int main(int argc, char *argv[]) {
-    size_t size = 0;
-    char *command = NULL;
-    if (argc > 1) {
-        char *filePath = argv[1];
-        if (filePath[0] == '-' && filePath[1] == '\0') {
-            command = readStdIn(&size);
-        } else {
-            command = readFile(filePath, &size);
-        }
-        if (command) {
-            ParseSendKeys(command, size);
-            free(command);
-            return 0;
-        }
+  size_t size = 0;
+  char *command = NULL;
+  if (argc > 1) {
+    char *filePath = argv[1];
+    if (filePath[0] == '-' && filePath[1] == '\0') {
+      command = readStdIn(&size);
+    } else {
+      command = readFile(filePath, &size);
     }
-    puts(HELP_TEXT);
-    return 1;
+    if (command) {
+      ParseSendKeys(command, size);
+      free(command);
+      return 0;
+    }
+  }
+  puts(HELP_TEXT);
+  return 1;
 }
